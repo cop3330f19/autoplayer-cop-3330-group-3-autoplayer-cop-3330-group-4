1cf2e5b8a6e8645e45aa3c9e465140f9b42f4c0d
CycoBlaze <61288200+CycoBlaze@users.noreply.github.com>
2020-04-25 15:44:01 -0400

    Update Song.cpp

--- Song.cpp
+++ Song.cpp
@@ -1,1 +1,82 @@
 
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include "Song.h"
+#include "Playlist.h"
+#include "StringHelper.h"
+
+using namespace std;
+
+//Constructors
+Song::Song()
+{
+    title = "";
+    artist = "";
+    album = "";
+    length = 0;
+    year = 0;
+}
+
+Song::Song(string title, string artist, string album, int length, int year)
+{
+    set(title, artist, album, length, year);
+}
+
+void Song::set(string title, string artist, string album, int length, int year)
+{
+    this->title = title;
+    this->artist = artist;
+    this->album = album;
+    this->length = length;
+    this->year = year;
+}
+
+ostream &operator<<(ostream &os, const Song &song)
+{
+    os << song.title << " " << song.artist << " " << song.album << " " << song.year << " " << song.length; 
+    return os;
+}
+
+ofstream &operator<<(ofstream &os, const Song &song)
+{
+    os << song.title << "," << song.artist << "," << song.album << "," << song.year << "," << song.length; 
+    return os;
+}
+
+fstream &operator>>(fstream &is, Song &song)
+{
+    string placeholder;
+
+    getline(is, song.title, ',');
+    getline(is, song.artist, ',');
+    getline(is, song.album, ',');
+    getline(is, placeholder, ',');
+    song.year = atoi(placeholder.c_str());
+    getline(is, placeholder);
+    song.length = atoi(placeholder.c_str());
+  
+    return is;
+}
+
+istream &operator>>(istream &is, Song &song)
+{
+    cout << "Song Details \n";
+    cout << "Title: \n";
+    getline(is, song.title);
+    cout << "Artist: \n";
+    getline(is, song.artist);
+    cout << "Album: \n";
+    getline(is, song.album);
+    cout << "Year: \n";
+    is >> song.year;
+    cout << "Length (in seconds): \n";
+    is >> song.length;
+    return is;
+}
+
+bool operator==(const Song &lhs, const Song &rhs)
+{
+    return (lhs.title == rhs.title && lhs.artist == rhs.artist); 
+}
