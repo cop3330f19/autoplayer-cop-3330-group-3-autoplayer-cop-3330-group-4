828a5dc2a09590711c64bd8b3fe163a751851f11
CycoBlaze <61288200+CycoBlaze@users.noreply.github.com>
2020-04-25 15:48:53 -0400

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -1,1 +1,176 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include "Playlist.h"
+#include "Song.h"
+#include "StringHelper.h"
 
+using namespace std;
+
+char Playlist::option = 'N'; 
+
+void Playlist::addSong(Song &s1)
+{
+    listOfSongs.push_back(s1); 
+}
+
+void Playlist::deleteSong(Song &deletesong)
+{
+
+    bool valid;
+    valid = false;
+    for (int iterator = 0; iterator < listOfSongs.size(); iterator++)
+    {
+        if (listOfSongs[iterator] == deletesong)
+        {
+            valid = true;
+            listOfSongs.erase(listOfSongs.begin() + iterator);
+        }
+    }
+}
+
+vector<Song> Playlist::getSong()
+{
+    return listOfSongs;
+}
+
+Playlist Playlist::intersectPlaylist(Playlist &p2)
+{
+
+    Playlist result;
+    bool valid;
+    vector<Song> temp = p2.getSong();
+
+    valid = false;
+
+    for (int iterator = 0; iterator < listOfSongs.size(); iterator++) 
+    {                                                                 
+        for (int forLoopIterator = 0; forLoopIterator < temp.size(); forLoopIterator++)
+        {
+            if (listOfSongs[iterator] == temp[forLoopIterator] && search(result.getSong(), listOfSongs[iterator]))
+
+                valid = true;
+
+            result.addSong(listOfSongs[iterator]);
+        }
+    }
+    return result;
+}
+
+bool Playlist::search(vector<Song> b, Song &g) 
+{
+    bool valid;
+
+    valid = false;
+    for (int iterator = 0; iterator < b.size(); iterator++)
+        if (b[iterator] == g)
+            valid = true;
+    return valid;
+}
+
+Playlist operator+(Playlist &p1, Playlist &p2)
+{
+    Playlist merge;
+    merge = p1;
+    bool valid;
+    vector<Song> temp = p2.getSong();
+
+    valid = false;
+
+    for (int iterator = 0; iterator < temp.size(); iterator++)
+    {
+
+        valid = true;
+
+        merge.addSong(temp[iterator]);
+    }
+    return Playlist(merge);
+}
+Playlist operator+(Playlist &p1, Song &s1)
+{
+    Playlist a1;
+    a1 = p1;
+    a1.addSong(s1);
+    return Playlist(a1);
+}
+
+Playlist operator-(Playlist &p1, Song &s1)
+{
+    Playlist d1;
+    d1 = p1;
+    d1.deleteSong(s1);
+    return d1;
+}
+
+ostream &operator<<(ostream &os, const Playlist &p1)
+{
+    for (int iterator = 0; iterator < p1.listOfSongs.size(); iterator++)
+    {
+        os << p1.listOfSongs[iterator];
+    }
+    return os;
+}
+
+void Playlist::play() 
+{
+    if (option == 'N' || option == 'n')
+    {
+        currentSongInPlaylist++;
+        if (currentSongInPlaylist < listOfSongs.size())
+            cout << listOfSongs[currentSongInPlaylist] << endl;
+        else
+
+            cout << "There are no more songs to play in the plalist." << endl;
+    }
+    else if (option == 'R' || option == 'r')
+    {
+        cout << listOfSongs[currentSongInPlaylist] << endl;
+    }
+    else if (option == 'L' || option == 'l')
+    {
+        currentSongInPlaylist++;
+        if (currentSongInPlaylist > listOfSongs.size())
+            currentSongInPlaylist = 0;
+        cout << listOfSongs[currentSongInPlaylist] << endl;
+    }
+}
+
+void Playlist::mode(char option)
+{
+    Playlist::option = option;
+}
+
+void Playlist::setPName(string pname)
+{
+    playlistName = pname;
+}
+
+string Playlist::getPlaylistname()
+{
+    return playlistName;
+}
+
+Playlist::Playlist()
+{
+    playlistName = "";
+}
+
+Playlist::Playlist(string name)
+{
+    setPName(name);
+    string play_listname = name + ".playlist";
+    play_listname = StringHelper::stou(play_listname);
+
+    fstream in;
+    in.open(play_listname.c_str(), ios::in);
+    Song s;
+    int k = 0;
+    while (in >> s)
+    {
+        listOfSongs.push_back(s);
+    }
+    in.close();
+}
