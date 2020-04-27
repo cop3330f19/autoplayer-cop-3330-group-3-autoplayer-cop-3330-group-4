1f8f780f3f55dff95c6a37e3bed71c323141070f
noahrich <nnrichardson1.14@gmail.com>
2020-04-26 21:39:57 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,7 @@
 //Player.cpp
 //Group 4 Noah Richardson, Milik Bennett-Spence, Desjah Thompson, Terik Hamilton
 //04/26/20
+//Allow users to create new named playlist, view all the playlists, merge (intersect) exisiting playlists into a new list, and play a playlist.
 #include <fstream>
 #include <iostream>
 #include <string>
